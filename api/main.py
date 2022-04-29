b0bf16fc122d41e17f7e6e8e72dc3807c048c908
iosvani1987 <iosvani1987@gmail.com>
2022-04-29 00:58:25 -0500

    Create Endpoint description in Homepage

--- api/main.py
+++ api/main.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 from flask_mysqldb import MySQL
 import os
 
-
 app = Flask(__name__)
 
 # MySQL configurations
@@ -14,9 +13,54 @@ app.config['MYSQL_PORT'] = 33066
 
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
-    return '<h1> Welcome !</h1>'
+def return_500_if_errors(f):
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except:
+            response = {
+                'status_code': 500,
+                'status': 'Internal Server Error'
+            }
+            return jsonify(response), 500
+    return wrapper
+
+@app.route('/', methods=['GET'])
+@return_500_if_errors
+def homepage():
+    return """
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+        <title>Welcome to Metrobus API</title>
+        </head>
+
+        <body>
+
+            <h1>Welcome to Metrobus API</h1>
+            <h2>Metrobus Endpoint:</h2>
+
+            <p>
+                This Endpoint is used to get the metrobuses list. <br>
+                https://localhost:5000/metrobuses
+            </p>
+            <p>
+                This Endpoint is used to get the metrobuses by vehicle_id. <br>
+                https://localhost:5000/metrobuses/<vehicle_id>
+            </p>
+            <p>
+                This Endpoint is used to get the metrobuses by vehicle_id. <br>
+                https://localhost:5000/metrobuses/<vehicle_id>
+            </p>
+            <p>
+                This Endpoint is used to get the metrobuses list. <br>
+                 https://localhost:5000/metrobuses
+            </p>
+
+        </body>
+    </html>
+    """
 
 # @app.route('/metrobuses', methods=['GET'])
 # def get_metrobuses():
@@ -38,7 +82,7 @@ def get_metrobuses_by_vehicle_id(vehicle_id = None):
             This route just admits the vehicle_id as integer parameter.
 
             This Endpoint is used to get the metrobuses list.
-#             https://localhost:5000/metrobuses
+            https://localhost:5000/metrobuses
     """
     cur = mysql.connection.cursor()
     if vehicle_id is not None:
